08437af68167a6e361d44ee80b366dbc911928c8
David Chi <davidchi31415@gmail.com>
2023-08-26 11:57:19 -0500

    Finished landing page

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -7,7 +7,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
 import { BsDiscord } from "react-icons/bs";
 import Image from "next/image";
-import { Badge } from "lucide-react";
+import { Badge } from "./ui/badge";
 
 const testimonials = [
     {
@@ -42,43 +42,51 @@ const testimonials = [
 
 export const LandingContent = () => {
     return (
-        <div className="py-10 md:py-20 bg-gradient-to-r from-[#e0e0e0] to-primary/50" >
+        <div className="py-10 md:py-20 bg-gradient-to-r from-[#e0e0e0] to-primary/10" >
             <div className="w-fit h-fit my-8 rounded-xl bg-[white] mx-auto">
                     <div className="text-center text-xl md:text-3xl p-4 w-fit rounded-xl bg-[#FFF1E4] border-4 border-[#FFD7AF]/50 shadow-md">
                         Becoming a singer has never been <b>so easy</b>.
                     </div>
                 </div>
-            <div className="mb-4 flex flex-col justify-center items-center gap-4 md:max-w-2xl mx-auto">
-                <div className="p-8 rounded-xl bg-white shadow-xl flex justify-between flex-wrap w-full gap-4">
-                    <div className="flex justify-center gap-2 mx-auto">
-                        <Image
-                            alt="record_step_picture"
-                            width={256} height={128}
-                            src="/record_step_picture.png"
-                        />
-                    </div>
-                    <div className="max-w-[16rem] mx-auto">
-                        <div className="text-2xl font-bold">1. Record Yourself</div>
-                        <p>
-                            Following a step-by-step, guided procedure, record your voice using
-                            your phone (or any high-quality microphone).
-                        </p>
+            <div className="flex justify-center items-center gap-4 flex-wrap">
+                <div className="pt-6">
+                    <div className="rounded-3xl relative flow-root max-w-sm h-[14rem] px-6 lg:pb-6 mx-auto border-4 border-[red]/25 bg-white">
+                        <div className="-mt-5">
+                            <div className="w-fit mx-auto">
+                                <span className="inline-flex items-center justify-center w-12 h-12 p-2 text-lg font-black text-white bg-red-500 rounded-full shadow">1</span>
+                            </div>
+                            <h3 className="mt-4 text-2xl font-bold leading-tight tracking-tight text-gray-700 text-center">Record yourself</h3>
+                            <p className="mt-2 text-lg">
+                                Using <u>just your phone</u>, record your voice by following our step-by-step voice cloning procedure - which takes <u>less than 15 minutes</u>!
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className="p-8 rounded-xl bg-white shadow-xl flex justify-between flex-wrap w-full gap-4">
-                    <div className="flex justify-center gap-2 mx-auto">
-                        <Image
-                            alt="upload_step_picture"
-                            width={256} height={128}
-                            src="/hero_step_2_picture.png"
-                        />
+                <div className="pt-6">
+                    <div className="rounded-3xl relative flow-root max-w-sm h-[14rem] px-6 lg:pb-6 mx-auto border-4 border-[green]/25 bg-white">
+                        <div className="-mt-5">
+                            <div className="w-fit mx-auto">
+                                <span className="inline-flex items-center justify-center w-12 h-12 p-2 text-lg font-black text-white bg-green-500 rounded-full shadow">2</span>
+                            </div>
+                            <h3 className="mt-4 text-2xl font-bold leading-tight tracking-tight text-gray-700 text-center">Upload and wait</h3>
+                            <p className="mt-2 text-lg">
+                                Once we have your recordings, we will construct a personal AI clone for your singing voice. In the meantime,
+                                find a song for your AI self to sing!
+                            </p>
+                        </div>
                     </div>
-                    <div className="max-w-[16rem] mx-auto">
-                        <div className="text-2xl font-bold">2. Upload and Wait</div>
-                        <p>
-                            Once we receive your recordings, we will construct a personal AI clone for your
-                            voice. And just like that, you're an Instant Singer!
-                        </p>
+                </div>
+                <div className="pt-6">
+                    <div className="rounded-3xl relative flow-root max-w-sm h-[14rem] px-6 lg:pb-6 mx-auto border-4 border-primary/25 bg-white">
+                        <div className="-mt-5">
+                            <div className="w-fit mx-auto">
+                                <span className="inline-flex items-center justify-center w-12 h-12 p-2 text-lg font-black text-white bg-primary rounded-full shadow">3</span>
+                            </div>
+                            <h3 className="mt-4 text-2xl font-bold leading-tight tracking-tight text-gray-700 text-center">Now, sing!</h3>
+                            <p className="mt-2 text-lg">
+                                Convert any song into your own voice, while keeping all the pitches and inflections of the original singer intact. You're now an instant singer!
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
