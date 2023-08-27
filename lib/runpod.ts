1fd6559885617afad733fc83421063f6988a0fc3
David Chi <davidchi31415@gmail.com>
2023-08-27 01:20:16 -0500

    Set epochs to 1

--- lib/runpod.ts
+++ lib/runpod.ts
@@ -281,7 +281,7 @@ export const _submitCloneJob = async ({
             "arguments": {
                 "input_id": jobId,
                 "model_id": modelId,
-                "num_epoch": 75,
+                "num_epoch": 1,
                 "save_every": 150,
                 "batch_size": 8,
                 "pitch_extraction_algorithm": "mangio-crepe",
