f4c20b9c07571fc5eaf156398b9aed4e54c5d7ff
David Chi <davidchi31415@gmail.com>
2023-09-03 23:24:40 -0500

    Switched dashboard back

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -2,11 +2,11 @@ import { CloneDashboard } from "./clone-dashboard"
 import ConvertDashboard from "./convert-dashboard"
 
 export const Dashboard = ({ userData }) => {
-    // if (userData.clone) {
+    if (userData.clone) {
         return (
             <ConvertDashboard userData={userData} />
         )
-    // }
+    }
 
     return (
         <CloneDashboard userData={userData} />
