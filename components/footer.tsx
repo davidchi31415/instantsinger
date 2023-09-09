293b3a8ad72cfdb88d3753ff8fd8b82f4002d9cf
David Chi <davidchi31415@gmail.com>
2023-09-09 11:27:11 -0500

    Improved footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <div className="py-8 bg-primary text-white font-bold">
             <div className="px-4 lg:px-8 grid lg:grid-cols-2 gap-8">
                 <div className="mx-auto lg:ml-16">
-                    <div className="bg-white text-primary text-lg p-4 rounded-full w-fit flex border-4 border-[gold]">
+                    <div className="bg-white text-primary text-lg p-4 rounded-full w-fit flex">
                         www.InstantSinger.com
                     </div>
                 </div>
