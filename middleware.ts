b9fe8b0b635b673aff16f0ee22765ba897f9b9d6
David Chi <davidchi31415@gmail.com>
2023-09-14 18:46:04 -0500

    Made song download endpoint temporarily public

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/api/convert/download/", "/result", "/pricing", "/contact", "/privacy", "/refund", "/tos", "/api/stripe/webhook", "/api/clone/webhook", "/api/convert/webhook"]
+  publicRoutes: ["/", "/api/convert/download-song", "/result", "/pricing", "/contact", "/privacy", "/refund", "/tos", "/api/stripe/webhook", "/api/clone/webhook", "/api/convert/webhook"]
 });
 
 export const config = {
