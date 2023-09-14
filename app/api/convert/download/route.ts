463cefc2f7c2e03e459f1aa12eabf0efe9b6ca06
David Chi <davidchi31415@gmail.com>
2023-09-14 18:38:02 -0500

    Implement song downloader endpoint

---
+++ app/api/convert/download/route.ts
@@ -0,0 +1,35 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export const runtime = 'edge';
+
+export async function POST(
+    req: Request
+) {
+    try {
+        const { userId } = auth();
+        const body = await req.json();
+        const { youtubeId } = body;
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+        if (!youtubeId) {
+            return new NextResponse("Need Youtube Id", { status: 400 });
+        }
+
+        const gcloudResponse = await fetch(
+            "https://us-central1-cosmic-axe-393519.cloudfunctions.net/song-downloader",
+            { method: "POST", headers: { "Content-type": "application/json" }, body: `youtubeId=${youtubeId}` }
+        );
+
+        if (gcloudResponse.status === 200) {
+            return new NextResponse("Song downloaded", { status: 200 });
+        }
+
+        return new NextResponse("Failed to downlaod", { status: 400 });
+    } catch (error) {
+        console.log("[SONG DOWNLOAD ERROR]", error);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+}
