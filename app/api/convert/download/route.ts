f2c8c83e86afe89865985ad73c0957fc42c428a9
David Chi <davidchi31415@gmail.com>
2023-09-14 18:56:17 -0500

    Replaced NextResposne with Response in download routes

--- app/api/convert/download/route.ts
+++ app/api/convert/download/route.ts
@@ -1,5 +1,3 @@
-import { NextResponse } from "next/server";
-
 export const runtime = 'edge';
 
 export async function POST(
@@ -14,7 +12,7 @@ export async function POST(
         //     return new NextResponse("Unauthorized", { status: 401 });
         // }
         if (!youtubeId) {
-            return new NextResponse("Need Youtube Id", { status: 400 });
+            return new Response("Need Youtube Id", { status: 400 });
         }
 
         const gcloudResponse = await fetch(
@@ -23,12 +21,12 @@ export async function POST(
         );
 
         if (gcloudResponse.status === 200) {
-            return new NextResponse("Song downloaded", { status: 200 });
+            return new Response("Song downloaded", { status: 200 });
         }
 
-        return new NextResponse("Failed to downlaod", { status: 400 });
+        return new Response("Failed to downlaod", { status: 400 });
     } catch (error) {
         console.log("[SONG DOWNLOAD ERROR]", error);
-        return new NextResponse("Internal error", { status: 500 });
+        return new Response("Internal error", { status: 500 });
     }
 }
