c801add05f4b57fc8c464c73bd8bfec826b90b7d
David Chi <davidchi31415@gmail.com>
2023-09-24 13:05:27 -0500

    Setup favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,26 @@ const rubik = Rubik({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Instant Singer',
   description: 'Become a singer in 2 minutes, using cutting-edge AI technology.',
+  icons: {
+    icon: [
+      { url: "/static/favicon-16x16.png", sizes: "16x16", type: "image" },
+      { url: "/static/favicon-32x32.png", sizes: "32x32", type: "image" }
+    ],
+    shortcut: "/static/favicon.ico",
+    apple: "/static/apple-touch-icon.png",
+    other: [
+      {
+        rel: "android-chrome",
+        url: "/static/android-chrome-192x192.png",
+        sizes: "192x192"
+      },
+      {
+        rel: "android-chrome",
+        url: "/static/android-chrome-512x512.png",
+        sizes: "512x512"
+      }
+    ]
+  }
 }
 
 
