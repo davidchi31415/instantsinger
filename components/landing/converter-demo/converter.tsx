434b261573d3d976dc48c542ce75f12131aa11db
David Chi <davidchi31415@gmail.com>
2023-09-27 13:41:06 -0500

    ..

--- components/landing/converter-demo/converter.tsx
+++ components/landing/converter-demo/converter.tsx
@@ -16,8 +16,8 @@ export const FakeConverterComponent = ({ onPlay }) => {
             <CardContent className="space-y-2">
                 <div className="mb-4 flex items-center gap-4">
                     <div>URL:</div>
-                    <div className="w-[12rem] md:w-[16rem]">
-                        <Input 
+                    <div className="w-[12rem] md:w-[16rem] pointer-events-none">
+                        <Input
                             placeholder="ex: https://youtube.com/..."
                             value="https://www.youtube.com/watch?v=pbNs7tAUFkk&pp=ygULa2VzaGkgZHJ1bms%3D"
                         />
