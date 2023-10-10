d74694b3d15660d01eb000957885c9d6fe233a14
David Chi <davidchi31415@gmail.com>
2023-10-10 13:30:29 -0500

    Set globalThis type checking code

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,7 +4,14 @@ declare global {
     var prisma: PrismaClient | undefined
 }
 
-const prismadb = globalThis.prisma || new PrismaClient();
-if (process.env.VERCEL_ENV !== "production") globalThis.prisma = prismadb;
+const prismadb = (typeof globalThis !== "undefined") ? (globalThis.prisma || new PrismaClient())
+    : (global.prisma || new PrismaClient());
+if (process.env.VERCEL_ENV !== "production") {
+    if (typeof globalThis !== "undefined") {
+        globalThis.prisma = prismadb;
+    } else {
+        global.prisma = prismadb;
+    }
+}
 
 export default prismadb;
